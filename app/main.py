1d6e153c48406778307c8ae778c94ee04c31d58f
ayoalabi <alabiayomide14@gmail.com>
2026-07-20 11:26:41 +0100

    Stage 3: create with validation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+class TaskCreate(BaseModel):
+    title: str
 
 tasks = [
     {"id": 1, "title": "Buy milk", "done": False},
@@ -7,6 +13,14 @@ tasks = [
 ]
 
 app = FastAPI()
+
+@app.exception_handler(RequestValidationError)
+def validation_error_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400,
+        content={"error": exc.errors()[0]["msg"]},
+    )
+
 
 @app.get("/")
 def read_root():
@@ -29,3 +43,11 @@ def get_task(task_id: int):
 @app.get("/tasks")
 def get_tasks():
     return tasks
+
+
+@app.post("/tasks", status_code=201)
+def create_task(new_task: TaskCreate):
+    next_id = max(task["id"] for task in tasks) + 1
+    task = {"id": next_id, "title": new_task.title, "done": False}
+    tasks.append(task)
+    return task
